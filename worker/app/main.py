44a293aa12d0b80b78d4629ab4408252c71c800c
Wojtek Zarzecki <wojtek.zarzecki2002@gmail.com>
2023-11-15 19:47:45 +0100

    feat: save result of job in cassandra

--- worker/app/main.py
+++ worker/app/main.py
@@ -5,35 +5,38 @@ import pika
 
 import logging
 
-# Create or get the logger
-logger = logging.getLogger('MyLogger')
-logger.setLevel(logging.INFO)  # Set the logging level
+from cassandra.cluster import Cluster
 
-# Create handler that logs to stdout
-stream_handler = logging.StreamHandler()
 
-# Create formatter and set it for the handler
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-stream_handler.setFormatter(formatter)
+def get_logger():
+    logger = logging.getLogger('MyLogger')
+    logger.setLevel(logging.INFO)  # Set the logging level
+    stream_handler = logging.StreamHandler()
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    stream_handler.setFormatter(formatter)
+    logger.addHandler(stream_handler)
+    return logger
 
-# Add the handler to the logger
-logger.addHandler(stream_handler)
 
-# Example usage
-logger.info('This is an info message')
+def get_cassandra_session():
+    cluster = Cluster(['cassandra'])
+    session = cluster.connect()
+    session.execute(
+        "CREATE KEYSPACE IF NOT EXISTS example WITH replication = {'class': 'SimpleStrategy', 'replication_factor' : 1};")
+    session.execute("USE example;")
+    return session
 
-def main():
+def main(logger=get_logger()):
     connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq', heartbeat=0))
-    # connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost', heartbeat=0))
-    print(1)
     channel = connection.channel()
-    print(2)
     channel.queue_declare(queue='hello')
 
+    cassandra_session = get_cassandra_session()
+
     def callback(ch, method, properties, body):
-        # with open(f"{uuid4()}.txt", "w") as f:
-        #     f.write(body)
-        logger.info(f" [x] Received {body}")
+        msg = f" [x] Received {body}"
+        logger.info(msg)
+        cassandra_session.execute("INSERT INTO hello_world (id, message) VALUES (%s, %s);", (uuid4(), msg))
         print(f" [x] Received {body}")
 
     channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
