75746efc441a665c4dece170f148b418797ead18
Wojtek Zarzecki <wojtek.zarzecki2002@gmail.com>
2023-12-03 21:23:37 +0100

    refactor: use repo pattern for saving file in api

--- api/app/data/AbractRepo.py
+++ api/app/data/AbractRepo.py
@@ -1,4 +1,8 @@
-# TODO use abstract class
-class AbstractRepo:
-    def save_file(filename: str) -> None:
+from abc import ABC, abstractmethod
+from fastapi import UploadFile
+
+
+class AbstractRepo(ABC):
+    @abstractmethod
+    async def save_file(self, file: UploadFile, filename: str) -> None:
         pass
