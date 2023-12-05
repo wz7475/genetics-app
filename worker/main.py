d54bd7b97609fb7535a5f681298c1e6dccb59288
Wojciech Zarzecki <wojtek.zarzecki2002@gmail.com>
2023-12-06 00:09:55 +0100

    Revert "feat: request triggers mock alg on uploaded file"
    This reverts commit 422b1be698ff735b661ba27c93d06c3b89fc4b1a.

--- worker/main.py
+++ worker/main.py
@@ -7,8 +7,7 @@ from app.PseudoRepoCassandra import get_cassandra_session
 from app.logger import get_logger
 from app.config import data_path
 from app.run_alg import run_alg
-from app.data import get_path
-
+temp_file_path = os.path.join(data_path, "temp_file.txt")
 
 def main(logger=get_logger()):
     connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq', heartbeat=0))
@@ -21,9 +20,7 @@ def main(logger=get_logger()):
         unique_id = properties.headers['unique_id']
         msg = f"{unique_id}: {body}"
         logger.info(msg)
-        input = get_path(f"{unique_id}.tsv")
-        output = get_path(f"{unique_id}_out.tsv")
-        run_alg(input, output)
+        run_alg(temp_file_path, temp_file_path + f"{unique_id}.txt")
         # read properties headers
         cassandra_session.execute("INSERT INTO hello_world (id, message) VALUES (%s, %s);", (uuid4(), msg))
 
@@ -34,4 +31,5 @@ def main(logger=get_logger()):
 
 if __name__ == '__main__':
     get_logger().info(os.listdir())
+    os.system("touch " + temp_file_path)
     main()
