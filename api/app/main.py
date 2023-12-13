0cefdfe6e3f0d877b620a9d951f139385f0128d7
Wojtek Zarzecki <wojtek.zarzecki2002@gmail.com>
2023-12-13 14:34:12 +0100

    fix: fix import

--- api/app/main.py
+++ api/app/main.py
@@ -33,10 +33,10 @@ def shutdown_event():
 @app.get("/")
 async def read_root():
     properties = pika.BasicProperties(headers={'unique_id': f"{uuid4()}"})
-    channel.basic_publish(exchange='',
-                          properties=properties,
-                          routing_key='hello',
-                          body='Hello World!')
+    # channel.basic_publish(exchange='',
+    #                       properties=properties,
+    #                       routing_key='hello',
+    #                       body='Hello World!')
     return {"message": "Job enqueued"}
 
 
--- api/tests/test_main.py
+++ api/tests/test_main.py
@@ -1,6 +1,6 @@
 from fastapi.testclient import TestClient
 
-from ..app.main import app
+from api.app.main import app
 
 client = TestClient(app)
 
@@ -8,4 +8,4 @@ client = TestClient(app)
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"Hello": "World"}
+    assert response.json() == {"message": "Job enqueued"}
