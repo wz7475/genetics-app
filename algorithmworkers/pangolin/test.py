34d4ee6434095f975b900fe13c00adedbda1a5e1
Wojtek Zarzecki <wojtek.zarzecki2002@gmail.com>
2024-01-14 14:29:30 +0100

    tests: update or add test for alg and map volumes

---
+++ algorithmworkers/pangolin/test.py
@@ -0,0 +1,4 @@
+from .main import Pangolin
+
+# ().process_task("./taskflowapi/shared_utils/data/spip.txt", "output.txt")
+Pangolin().process_task("/code/data_test/100.tsv", "/code/data_test/output.tsv")
--- algorithmworkers/spip/test.py
+++ algorithmworkers/spip/test.py
@@ -1,4 +1,4 @@
 from .main import SPIP
 
 # SPIP().process_task("./taskflowapi/shared_utils/data/spip.txt", "output.txt")
-SPIP().process_task("/code/data/100.tsv", "/code/data/output.tsv")
+SPIP().process_task("/code/data_test/100.tsv", "/code/data_test/output.tsv")
