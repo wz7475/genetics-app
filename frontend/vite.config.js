279263fc39f9e76f05c16ed5554ccd58b990e3ce
Adam Jeliński <adas.jelinski@gmail.com>
2024-01-16 01:45:44 +0100

    build: add developement api proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -29,5 +29,13 @@ export default defineConfig({
     },
     server: {
         port: 3000,
+        proxy: {
+            '/api': {
+                target: 'http://localhost:8000',
+                changeOrigin: true,
+                secure: false,
+                rewrite: (path) => path.replace(/^\/api/, ''),
+            },
+        },
     },
 })
