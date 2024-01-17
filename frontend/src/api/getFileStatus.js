65e42dd762033d282bc55a0ca086fb07e8d2aadc
gambolkf <gambolkf@gmail.com>
2024-01-17 14:23:58 +0100

    bug: changed api to send strings not jsons

--- frontend/src/api/getFileStatus.js
+++ frontend/src/api/getFileStatus.js
@@ -1,8 +1,6 @@
 export const getFileStatus = async (taskId) => {
     return await fetch('api/getStatus', {
         method: 'POST',
-        body: JSON.stringify({
-            task_id: taskId,
-        }),
+        body: taskId,
     }).then((res) => res.json())
 }
