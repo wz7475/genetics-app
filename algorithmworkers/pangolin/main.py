bff1804b380b3d0fa7dabe904d0f0dbcad1816b9
Rafał <szczepaniak.rafal@gmail.com>
2024-01-17 12:49:12 +0100

    Add tests/ directory

--- algorithmworkers/pangolin/main.py
+++ algorithmworkers/pangolin/main.py
@@ -21,7 +21,7 @@ class Pangolin(Algorithm):
                 self.get_alg_input_name(),
                 "GRCh38.primary_assembly.genome.fa.gz",
                 "gencode.v38.annotation.db",
-                os.path.join(self.tmp_dir_name, "output"),
+                os.path.join(self.tmp_dir_name, "output")
             ],
             stdout=subprocess.PIPE,
         )
@@ -33,44 +33,44 @@ class Pangolin(Algorithm):
         return process.returncode
 
     def prepare_input(self, input_file_path):
-        with open(input_file_path) as input_file:
-            with open(self.get_alg_input_name(), mode="w") as alg_input_file:
-                source = csv.DictReader(input_file, delimiter="\t")
-                dest = csv.DictWriter(
-                    alg_input_file,
-                    delimiter=",",
-                    fieldnames=["CHROM", "POS", "REF", "ALT"],
+        with open(input_file_path) as input_file, open(self.get_alg_input_name(), mode="w") as alg_input_file:
+            source = csv.DictReader(input_file, delimiter="\t")
+            dest = csv.DictWriter(
+                alg_input_file,
+                delimiter=",",
+                fieldnames=["CHROM", "POS", "REF", "ALT"],
+            )
+
+            dest.writeheader()
+            for row in source:
+                chromosome = row["Chr"][3:]
+                position = row["POS"]
+                reference = row["Ref"]
+                alternative = row["Alt"]
+
+                dest.writerow(
+                    {
+                        "CHROM": chromosome,
+                        "POS": position,
+                        "REF": reference,
+                        "ALT": alternative,
+                    }
                 )
-
-                dest.writeheader()
-                for row in source:
-                    chromosome = row["Chr"][3:]
-                    position = row["POS"]
-                    reference = row["Ref"]
-                    alternative = row["Alt"]
-
-                    dest.writerow(
-                        {
-                            "CHROM": chromosome,
-                            "POS": position,
-                            "REF": reference,
-                            "ALT": alternative,
-                        }
-                    )
 
     def prepare_output(self, output_file_path):
-        with open(output_file_path, mode="w") as output_file:
-            with open(self.get_alg_output_name()) as alg_output_file:
-                source = csv.DictReader(alg_output_file, delimiter=",")
-                dest = csv.DictWriter(
-                    output_file,
-                    delimiter="\t",
-                    fieldnames=[self.name],
-                )
-                dest.writeheader()
-                for row in source:
-                    result = row["Pangolin"]
-                    dest.writerow({self.name: result})
+        with open(output_file_path, mode="w") as output_file, open(self.get_alg_output_name()) as alg_output_file:
+            source = csv.DictReader(alg_output_file, delimiter=",")
+            dest = csv.DictWriter(
+                output_file,
+                delimiter="\t",
+                fieldnames=[self.name],
+            )
+
+            dest.writeheader()
+            for row in source:
+                result = row["Pangolin"]
+
+                dest.writerow({self.name: result})
 
 
 if __name__ == "__main__":
---
+++ tests/config.py
@@ -0,0 +1,3 @@
+import os
+
+PATH_TO_DATA = os.path.join('tests', 'data')
