5dd54f1cda4998e3672c7f1ec0a94b22ee62b0ce
gambolkf <gambolkf@gmail.com>
2024-01-22 22:48:07 +0100

    tests: added initial test impl, doesnt work because of the way of mounting logger and other packages

--- tests/test_api/test_api.py
+++ tests/test_api/test_api.py
@@ -1,26 +1,30 @@
-# from fastapi.testclient import TestClient
-#
-# from taskflowapi.api.app.main import app
-#
-# testclient = TestClient(app)
+from fastapi.testclient import TestClient
+
+from taskflowapi.api.app.main import app
+
+testclient = TestClient(app)
 
 
-def test_sanity_check():
-    assert 1 == 1
-#
-# def test_simply_endpoint():
-#     response = testclient.get("/test")
-#     assert response.status_code == 200
-#     assert response.json() == {"message": "Job enqueued"}
-#
-#
-# def test_startup():
-#     """
-#     - context manager to include on event startup and shutdown
-#     - requires a rabbitmq server running / mocking it
-#     """
-#     # with TestClient(app) as client:
-#     #     response = client.get("/")
-#     #     assert response.status_code == 200
-#     #     assert response.json() == {"message": "Job enqueued"}
-#     assert "app" == "app"
+
+def test_startup():
+    """
+    - context manager to include on event startup and shutdown
+    - requires a rabbitmq server running / mocking it
+    """
+    # with TestClient(app) as client:
+    #     response = client.get("/")
+    #     assert response.status_code == 200
+    #     assert response.json() == {"message": "Job enqueued"}
+    assert "app" == "app"
+
+
+
+
+def test_algorythm():
+    """
+    - requiers diffrent method of using sheared utils in app
+    """
+    # with TestClient(app) as client:
+    #     response = client.get("/availableAlgorithms")
+    #     assert response.status_code == 200
+    #     assert response.json() == {"message": "Job enqueued"}
