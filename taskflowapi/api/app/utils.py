ea5a664a79e5faf4fa0f7e36eb3926c485a7c6e2
rszczep2 <01169247@pw.edu.pl>
2024-02-08 00:29:21 +0100

    Fix operations on UploadFile

--- taskflowapi/api/app/utils.py
+++ taskflowapi/api/app/utils.py
@@ -1,7 +1,13 @@
 from uuid import uuid4
 from io import IOBase
 
-from config import LIST_OF_COLUMNS_PANGOLIN, LIST_OF_COLUMNS_SPIP
+from .config import LIST_OF_COLUMNS_PANGOLIN, LIST_OF_COLUMNS_SPIP
+
+
+class MissingColumnException(Exception):
+    def __init__(self, column_name):
+        self.message = f"Input file is missing '{column_name}' column"
+        super().__init__(self.message)
 
 
 def count_file_lines(path: str) -> int:
@@ -22,17 +28,17 @@ def validate_input_file(input_file: IOBase, algorithms: list[str]) -> None:
         list_of_obligatory_columns.extend(LIST_OF_COLUMNS_SPIP)
 
     num_columns = len(list_of_obligatory_columns)
-    columns_string = input_file.readlines(1)[0].replace('\n', '')
+    columns_string = input_file.readlines(1)[0].decode('utf-8').replace('\n', '')
 
     for obligatory_column in list_of_obligatory_columns:
         if obligatory_column not in columns_string.split('\t'):
-            raise AttributeError(obligatory_column)
+            raise MissingColumnException(obligatory_column)
 
     number_of_tabulators_in_row = columns_string.count('\t')
     if any([delimiter in columns_string for delimiter in [' ', ',', ', ']]) or number_of_tabulators_in_row < num_columns - 1:
         raise ValueError("Input file should only have tabulators as delimiters")
 
     for row in input_file.readlines():
-        if row.count('\t') != number_of_tabulators_in_row:
+        if row.decode('utf-8').count('\t') != number_of_tabulators_in_row:
             raise ValueError("Input file should have tabulators as delimiters, " +
                              "and all rows should have same amount of columns")
